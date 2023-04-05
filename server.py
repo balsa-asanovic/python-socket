004c403b4874e2525839f7d71fb79bfbb6e3a58c
balsa-asanovic <balsaasanovic95@gmail.com>
2023-04-06 01:22:23 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,50 @@
+import time
+import threading
+import random
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins='https://imaginative-cat-228fb6.netlify.app/')
+
+emitting = False
+frequency = 10000
+thread = None
+
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response(status, headers)
+    return [b"Hello World!"]
+
+def emit_message():
+    global emitting
+    global frequency
+    while emitting:
+        socketio.emit('message', random.uniform(0.1, 2))
+        time.sleep(frequency / 1000)
+
+@socketio.on('connect')
+def handle_connect():
+    global emitting
+    print('Client connected')
+    if not emitting:
+        emitting = True
+        thread = threading.Thread(target=emit_message)
+        thread.start()
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+
+@socketio.on('frequency')
+def handle_message(data):
+    global frequency
+    frequency = data
+    print(f'Received message: {data}')
+
+if __name__ == '__main__':
+    socketio.run(app, host='localhost', port=8080)
+
