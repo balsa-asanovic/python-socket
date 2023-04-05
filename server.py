505775e570f83b880b233e9b137ea24092b2522f
balsa-asanovic <balsaasanovic95@gmail.com>
2023-04-06 01:36:30 +0200

    added CORS

--- server.py
+++ server.py
@@ -3,8 +3,10 @@ import threading
 import random
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins='https://imaginative-cat-228fb6.netlify.app/')
 
